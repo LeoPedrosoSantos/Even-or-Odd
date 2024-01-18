cf9908e6d83765f13d521ab79dd48665775e3aa6
Leonardo Pedroso dos Santos <leonardopedroso27@yahoo.com.br>
2024-01-18 20:05:23 -0300

    Even or Odd number check

---
+++ Odd or even.py
@@ -0,0 +1,10 @@
+print("***Odd or even number check***\n")
+
+number = int(input("What number do you want to check?\n"))
+
+if number % 2 != 0:
+    print("Odd")
+else:
+    print("Even")
+
+print(input("CLick ENTER to close"))
